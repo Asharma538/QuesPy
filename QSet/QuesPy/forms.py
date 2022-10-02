be1138d78f826bc2b354cb04f1bb4ebf50e7da10
Anadi Sharma <sharma.130@iitj.ac.in>
2022-10-03 01:21:52 +0530

    fixed the image requrement problem

--- QSet/QuesPy/forms.py
+++ QSet/QuesPy/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class MyFileUploadForm(forms.Form):
-    image_q = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control'}))
-    image_op1 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op1img'}))
-    image_op2 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op2img'}))
-    image_op3 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op3img'}))
-    image_op4 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op4img'}))
+    image_q = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control'}),required=False)
+    image_op1 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op1img'}),required=False)
+    image_op2 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op2img'}),required=False)
+    image_op3 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op3img'}),required=False)
+    image_op4 = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control op4img'}),required=False)
